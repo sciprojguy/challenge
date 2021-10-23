a134f68f97e8e04560cabe211882bbbc08b123dc
Chris Woodard <sciprojguy@gmail.com>
2021-10-23 11:56:15 -0400

    Initial Commit

---
+++ TraegerChallenge/TraegerChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraegerChallengeApp.swift
+//  TraegerChallenge
+//
+//  Created by Admin on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TraegerChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
