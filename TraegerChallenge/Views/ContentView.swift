7db25b805d63d099a486c3a1584691a89dae3a74
Chris Woodard <sciprojguy@gmail.com>
2021-10-30 09:28:14 -0400

    updated UI to make it a little nicer part deux

--- TraegerChallenge/Views/ContentView.swift
+++ TraegerChallenge/Views/ContentView.swift
@@ -15,46 +15,37 @@ struct ContentView: View {
     
     var body: some View {
 
-        ZStack {
-            VStack {
-                Text("Astronomical Pic of the Day").font(.largeTitle)
-                if apodItems.items.count < 1 {
-                    Text("No APODs available")
-                }
-                else {
-                    //map items we get back from API to view model items
-                    List(apodItems.items.map {
-                        APODItemViewModel(
-                            formattedDate: $0.date,
-                            itemTitle: $0.title,
-                            itemUrl: $0.url,
-                            itemImageName: URL(string: $0.url)?.lastPathComponent ?? "",
-                            itemDescription: $0.explanation
-                        )}, id:\.self) { item in
-                        ListItem(item: item).onTapGesture {
-                            if let imgName = URL(string: item.itemUrl)?.lastPathComponent {
-                                //start image download if it's not already downloaded
-                                if nil == apodImgFetcher.capturedImage(imgName: imgName) {
-                                    apodImgFetcher.fetchAPODImage(urlStr: item.itemUrl)
-                                }
-                                else {
-                                    apodImgFetcher.imageName = imgName
-                                }
-                                self.detailItem = item
-                            }
-                        }
-                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
-                }
-            }.sheet(item: $detailItem) { item in
-                ItemDetail(item: item)
+        VStack {
+            Text("Astronomical Pic of the Day").font(.largeTitle)
+            if apodItems.items.count < 1 {
+                Text("No APODs available")
             }
-            
-//
-//            if nil != self.detailItem {
-//                if let imgName = self.apodImgFetcher.imageName {
-//                    ItemDetail(item: self.detailItem!)
-//                }
-//            }
+            else {
+                //map items we get back from API to view model items
+                List(apodItems.items.map {
+                    APODItemViewModel(
+                        formattedDate: $0.date,
+                        itemTitle: $0.title,
+                        itemUrl: $0.url,
+                        itemImageName: URL(string: $0.url)?.lastPathComponent ?? "",
+                        itemDescription: $0.explanation
+                    )}, id:\.self) { item in
+                    ListItem(item: item).onTapGesture {
+                        if let imgName = URL(string: item.itemUrl)?.lastPathComponent {
+                            //start image download if it's not already downloaded
+                            if nil == apodImgFetcher.capturedImage(imgName: imgName) {
+                                apodImgFetcher.fetchAPODImage(urlStr: item.itemUrl)
+                            }
+                            else {
+                                apodImgFetcher.imageName = imgName
+                            }
+                            self.detailItem = item
+                        }
+                    }
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }.sheet(item: $detailItem) { item in
+            ItemDetail(item: item)
         }
         .onAppear(perform:  {apodItems.fetch7MostRecentAPODs()})
     }
